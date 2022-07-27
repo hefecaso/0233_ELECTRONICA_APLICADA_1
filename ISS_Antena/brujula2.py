178f1e83337d31c43761569897e1d157cb3b420a
hefecaso <hfcarrerasoto@ieee.org>
2022-07-27 01:33:54 -0600

    Eliminado while

--- ISS_Antena/brujula2.py
+++ ISS_Antena/brujula2.py
@@ -36,26 +36,3 @@ def pos():
     return posicion
 
 posicion = get2_heading(sensor)
-
-
-while True:
-
-    print("Azimut: {:.2f} grados".format(get_heading(sensor))+"----Elevación: {:.2f} grados".format(get2_heading(sensor)))
-    time.sleep(0.5)
-    screen=turtle.Screen()
-    screen.title("Azimut & Elevación")
-    screen.setup(640, 640, 0, 0)
-    screen.bgpic('brujula3.gif')
-    showturtle()
-    turtle.rt(get_heading(sensor))
-    turtle.fd(100)
-    time.sleep(0.5)
-    turtle.home()
-    time.sleep(0.9)
-    turtle.rt(get2_heading(sensor))
-    pencolor("red")
-    turtle.fd(100)
-    time.sleep(0.5)
-    turtle.home()
-    time.sleep(0.5)
-    pencolor("cyan")
