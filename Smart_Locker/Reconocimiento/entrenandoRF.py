a528a7edbc5562e0cfda6728011f02b4321e12a9
hefecaso <hfcarrerasoto@yahoo.es>
2022-10-04 22:53:38 -0600

    Reconocimiento

---
+++ Smart_Locker/Reconocimiento/entrenandoRF.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'Yo' #Cambia a la ruta donde hayas almacenado Data
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv2.imread(personPath+'/'+fileName,0))
+
+	label = label + 1
+
+
+
+# Métodos para entrenar el reconocedor
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+# Almacenando el modelo obtenido
+
+face_recognizer.write('modeloLBPHFace.xml')
+print("Modelo almacenado...")
