d0bf02900f527e6ba6351488d8de77c9353ae69d
chrisfrrrr <chrisfranco93@gmail.com>
2022-10-31 16:31:33 -0600

    AGREGANDO PROGRAMA RECOLECCIÓN DE DATOS

---
+++ Smart_Locker/Reconocimiento/prueba1.py
@@ -0,0 +1,14 @@
+import getpass
+from numpy import *
+import matplotlib.pyplot as plt
+import pandas as pd
+
+Nombre = input("Escriba su nombre ")
+Contraseña = getpass.getpass("Ingrese su contraseña ")
+Locker = int(input("Digite el numero de locker que desea "))
+Dpi = int(input("Digite su numero de identificacion personal "))
+
+#savetxt("Nombre.csv", array([Nombre,Contraseña,Locker,Dpi]).T , delimiter=",", header='Nombre,Contraseña,Locker,Dpi')
+
+df = pd.DataFrame([[Nombre,Locker,Dpi]], columns = ['Nombre', 'Locker', 'Dpi'])
+df.to_csv(Nombre)
